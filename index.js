c6be322a09de52e43c5ffef26f3e2e57d213600b
Yan Porfirio <yanporfirio13@gmail.com>
2026-05-11 13:18:08 -0300

    refactor: replace day-of-week logic with area calculation function in index.js

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-let agora = new Date()
-let diaSem = agora.getDay()
-
-diaSem = 4
-
-switch (diaSem) {
-    case 0:
-        console.log('Domingo')
-        break
-    case 1:
-        console.log('Segunda')
-        break
-    case 2:
-        console.log('Terça')
-        break
-    case 3:
-        console.log('Quarta')
-        break
-    case 4:
-        console.log('Quinta')
-        break
-    case 5:
-        console.log('Sexta')
-        break
-    case 6:
-        console.log('Sábado')
-        break
-    default:
-        console.log('[ERRO] Dia Inválido!') 
+const area = (base, altura) => {
+    return base * altura
 }
+
+console.log(area(12, 15) + 'cm².')
